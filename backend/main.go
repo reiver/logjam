c7d5441b7927edffe621d97ada16b1b12be95f52
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-07-14 15:47:36 -0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/sparkscience/logjam/backend/srv/log"
+	
 	"fmt"
 )
 
 func main() {
+	log := logsrv.Begin()
+	defer log.End()
+	
 	fmt.Println("Hello world! — I am logjam! ✨")
 }
