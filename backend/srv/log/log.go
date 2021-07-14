a942abb7e60486014a75fffc50076705ce403db9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-07-14 15:45:49 -0700

    Update log.go

--- backend/srv/log/log.go
+++ backend/srv/log/log.go
@@ -1,3 +1,17 @@
 package logsrv
 
-//@TODO
+import (
+	"github.com/mmcomp/go-log"
+
+	"os"
+)
+
+var (
+	Logger log.Logger = log.Logger{
+		Output: os.Stdout,	
+	}
+)
+
+func Begin(a ...interface{}) log.Logger {
+	return Logger.Begin(a...)
+}
