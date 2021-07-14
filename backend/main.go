aa04ca2e43a08f37ac6001f43b0e63a13c282f15
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-07-14 16:50:16 -0700

    --test-logs flag

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"github.com/sparkscience/logjam/backend/arg"
 	"github.com/sparkscience/logjam/backend/srv/log"
-	
+
 	"fmt"
 )
 
 func main() {
 	log := logsrv.Begin()
 	defer log.End()
-	
+
+	// If --test-logs flag was used, then do a logs test and then exit.
+	if arg.TestLogs {
+		log.Trace("trace test")
+		log.Log("log test")
+		log.Inform("inform test")
+		log.Highlight("highlight test")
+		log.Warn("warn test")
+		log.Alert("alert test")
+		log.Error("error test")
+/////////////// RETURN
+		return
+	}
+
 	fmt.Println("Hello world! — I am logjam! ✨")
 }
