6dcc7b84b7d1460af9b0b9699fc5d6521af148b0
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-07-14 09:55:09 -0700

    Create arg.go

---
+++ backend/arg/arg.go
@@ -0,0 +1,31 @@
+package arg
+
+import (
+	"flag"
+)
+
+var (
+	Values []string
+)
+
+var (
+	Verbose bool
+	VeryVerbose bool
+	VeryVeryVerbose bool
+	VeryVeryVeryVerbose bool
+	VeryVeryVeryVeryVerbose bool
+	VeryVeryVeryVeryVeryVerbose bool
+)
+
+func init() {
+	flag.BoolVar(&Verbose,                     "v",      false,                          "verbose logs outputted")
+	flag.BoolVar(&VeryVerbose,                 "vv",     false,                     "very verbose logs outputted")
+	flag.BoolVar(&VeryVeryVerbose,             "vvv",    false,                "very very verbose logs outputted")
+	flag.BoolVar(&VeryVeryVeryVerbose,         "vvvv",   false,           "very very very verbose logs outputted")
+	flag.BoolVar(&VeryVeryVeryVeryVerbose,     "vvvvv",  false,      "very very very very verbose logs outputted")
+	flag.BoolVar(&VeryVeryVeryVeryVeryVerbose, "vvvvvv", false, "very very very very very verbose logs outputted")
+
+	flag.Parse()
+	
+	Values = flag.Args()
+}
