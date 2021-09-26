f054f204d8b9069a9edd890713e07c4f9a970d6b
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2021-09-26 18:32:27 +0330

    add logs endpoint

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ Private repo for logjam.
 **DO NOT SHARE THIS!**
 
 # Endpoints 
-Reset connections :
-```
-/v1/connections
-```
-
 Get Status of nodes
 ```
 /v1/connections/graph
+```
+
+View logs
+```
+/logs
 ```
--- backend/lib/handlers/logsHandler/logshandler.go
+++ backend/lib/handlers/logsHandler/logshandler.go
@@ -1,10 +1,8 @@
-package statichandler
+package logshandler
 
 import (
 	"net/http"
 )
-
-// var Handler http.Handler = http.FileServer(http.Dir("./public/statics/files"))
 
 func Handler(path string) http.Handler {
 	return http.FileServer(http.Dir(path))
