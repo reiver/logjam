07d85101f7076270783a7f945102537b469df67e
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2022-01-30 10:23:03 +0330

    try on no node to connect and show error

--- backend/lib/handlers/websockethandler/websockethandler.go
+++ backend/lib/handlers/websockethandler/websockethandler.go
@@ -161,10 +161,9 @@ func (receiver httpHandler) parseMessage(socket *binarytreesrv.MySocket, message
 					msg := "Audiance " + socket.Name + " could not receive stream because there is no broadcaster now!"
 					fmt.Fprintln(f, msg)
 				} else {
-					// broadcaster = broadcasterLevel[0].(*binarytreesrv.MySocket)
 					var broadResponse message.MessageContract
 					broadResponse.Type = "add_audience"
-					broadResponse.Data = strconv.FormatInt(int64(socket.ID), 10) // + "," + socket.Name
+					broadResponse.Data = strconv.FormatInt(int64(socket.ID), 10)
 					broadResponseJSON, err := json.Marshal(broadResponse)
 
 					msg := "Audiance " + socket.Name + " is going to get connected"
@@ -172,9 +171,11 @@ func (receiver httpHandler) parseMessage(socket *binarytreesrv.MySocket, message
 
 					if err == nil {
 						log.Highlight("Deciding to connect ...")
-						targetSocketNode, e := Map.InsertChild(socket.Socket, false) // receiver.decideWhomToConnect(broadcaster)
+						targetSocketNode, e := binarytreesrv.InsertChild(socket.Socket, Map)
+						// targetSocketNode, e := Map.InsertChild(socket.Socket, false)
 						if e != nil {
 							log.Error("Insert Child Error ", e)
+							socket.Socket.WriteMessage(messageType, []byte("{\"type\":\"error\",\"data\":\"Insert Child Error : "+e.Error()+" \"}"))
 							return
 						}
 						log.Highlight("Deciding to connect end")
@@ -187,17 +188,7 @@ func (receiver httpHandler) parseMessage(socket *binarytreesrv.MySocket, message
 						if targetSocket.Socket != broadcaster.Socket {
 							broadResponse.Type = "add_broadcast_audience"
 						}
-						// websocketmap.Map.InsertConnected(targetSocket.Socket, socket.Socket)
-						// log.Informf("Target Socket has %d sockets connected!", len(websocketmap.Map.Get(targetSocket.Socket).ConnectedSockets))
 						targetSocket.Socket.WriteMessage(messageType, broadResponseJSON)
-						level := 1
-						for {
-							if len(Map.LevelNodes(uint(level))) == 0 {
-								break
-							}
-							// log.Highlightf("Level %d: %d", level, len(receiver.levelSockets(uint(level))))
-							level++
-						}
 					} else {
 						log.Error("Marshal Error of `add_audience` broadResponse", err)
 						return
@@ -214,7 +205,6 @@ func (receiver httpHandler) parseMessage(socket *binarytreesrv.MySocket, message
 		}
 	case "stream":
 		log.Alert("Stream Received ", socket.Name)
-		// websocketmap.Map.SetStreamState(socket.Socket, true)
 		Map.ToggleCanConnect(socket.Socket)
 
 		msg := "Audiance " + socket.Name + " is receiving stream!"
