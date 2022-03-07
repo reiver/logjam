f2f43f53d865f93232b1489f4034d5f4ff702eec
accesstoken <massoud77@gmail.com>
2022-03-06 22:43:12 -0800

    added camera click handler

--- frontend/ui.js
+++ frontend/ui.js
@@ -1,16 +1,35 @@
+const CAMERA_ON = "images/cam-on.png";
+const CAMERA_OFF = "images/cam-off.png";
+const MIC_ON = "images/mic-on.png";
+const MIC_OFF = "images/mic-off.png";
+const SPARK_LOGO = "images/spark-logo.png";
+
 function arrangeVideoContainers() {
-    const videoContainers = document.getElementById('screen').getElementsByClassName('video-container');
+    const videoContainers = document.getElementById('screen')
+        .getElementsByClassName('video-container');
     const videoCount = videoContainers.length;
     const flexGap = 1;
     let flexRatio = 100 / Math.ceil(Math.sqrt(videoCount));
     let flex = "0 0 calc(" + flexRatio + "% - " + flexGap + "px)";
-    videoContainers.forEach(div => {
+
+    Array.from(videoContainers).forEach(div => {
             div.style.setProperty('flex', flex);
         }
     )
 }
 
+function onCameraButtonClick() {
+    const img = document.getElementById("camera");
+    if (img.dataset.status === 'on') {
+        img.dataset.status = 'off';
+        img.src = CAMERA_OFF;
+    }else{
+        img.dataset.status = 'on';
+        img.src = CAMERA_ON;
+    }
+}
 
-function onLoad(){
+
+function onLoad() {
     arrangeVideoContainers();
 }
