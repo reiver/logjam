eebdabccfa23601cdb447b24bdf8d168cf82797c
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2022-03-13 12:36:51 +0330

    add ui demo

---
+++ backend/public/statics/files/frontend/js/spark-rtc.js
@@ -0,0 +1,263 @@
+class SparkRTC {
+    myPeerConnectionConfig = {
+        iceServers: [
+            {
+                urls: 'stun:stun.l.google.com:19302'
+            },
+            {
+                urls: 'stun:stun1.l.google.com:19302'
+            },
+            {
+
+                urls: 'stun:stun2.l.google.com:19302'
+            },
+            {
+
+                urls: 'stun:stun3.l.google.com:19302'
+            },
+            {
+
+                urls: 'stun:stun4.l.google.com:19302'
+            },
+            {
+                urls: "turn:turn1.turn.group.video:3478",
+                username: "turnuser",
+                credential: "dJ4kP05PHcKN8Ubu",
+            },
+            {
+                urls: "turn:turn2.turn.group.video:3478",
+                username: "turnuser",
+                credential: "XzfVP8cpNEy17hws",
+            },
+            {
+                urls: "turns:turn1.turn.group.video:443",
+                username: "turnuser",
+                credential: "dJ4kP05PHcKN8Ubu",
+            },
+            {
+                urls: "turns:turn2.turn.group.video:443",
+                username: "turnuser",
+                credential: "XzfVP8cpNEy17hws",
+            },
+        ],
+    };
+
+    role = 'broadcast';
+    localStream;
+    socket;
+    myName = 'NoName';
+    myUsername = 'NoUsername';
+    myPeerConnectionArray = {};
+    iceCandidates = [];
+    handleVideoOfferMsg = async (msg) => {
+        const broadcasterPeerConnection = this.myPeerConnectionArray[msg.name] || this.newPeerConnectionInstance(msg.name);
+        await broadcasterPeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        await broadcasterPeerConnection.setLocalDescription(await broadcasterPeerConnection.createAnswer());
+
+        this.socket.send(
+            JSON.stringify({
+                name: this.myUsername,
+                target: msg.name,
+                type: "video-answer",
+                sdp: broadcasterPeerConnection.localDescription,
+            })
+        );
+    }
+    handleMessage = async (event) => {
+        let msg;
+        try {
+            msg = JSON.parse(event.data);
+        } catch (e) {
+            return;
+        }
+        msg.data = (msg.Data && !msg.data) ? msg.Data : msg.data;
+        msg.type = (msg.Type && !msg.type) ? msg.Type : msg.type;
+
+        if (msg.type !== 'new-ice-candidate') console.log(msg);
+        let audiencePeerConnection;
+        switch (msg.type) {
+            case 'video-offer':
+                this.handleVideoOfferMsg(msg);
+                break;
+            case 'video-answer':
+                console.log('Got answer.', msg);
+                audiencePeerConnection = this.createOrGetPeerConnection(msg.data);
+                try {
+                    await audiencePeerConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+                } catch (e) {
+                    console.log('setRemoteDescription failed with exception: ' + e.message);
+                    console.log(audiencePeerConnection);
+                    console.log(msg.sdp);
+                }
+                break;
+            case 'new-ice-candidate':
+                console.log('Got ICE candidate.', msg);
+                audiencePeerConnection = this.createOrGetPeerConnection(msg.data);
+                this.iceCandidates.push(new RTCIceCandidate(msg.candidate));
+                if (audiencePeerConnection && audiencePeerConnection.remoteDescription) {
+                    audiencePeerConnection.addIceCandidate(this.iceCandidates.pop());
+                }
+                break;
+            case 'role':
+                if (this.role === 'broadcast') {
+                    if (msg.data === "no:broadcast") {
+                        alert("You are not a broadcaster anymore!");
+                        this.socket.close();
+                    } else if (msg.data === "yes:broadcast") {
+                        this.localStreamChangeCallback(this.localStream);
+                    } else {
+                        this.localStreamChangeCallback(null);
+                    }
+                }
+                break;
+            case 'start':
+                if (msg.error) {
+                    alert(msg.error);
+                    return;
+                }
+
+                this.myUsername = msg.data;
+                break;
+            case 'add_audience':
+            case 'add_broadcast_audience':
+                this.connectToAudience(msg.data);
+                break;
+            default:
+                break;
+        }
+    };
+    setupSignalingSocket = async (url, myName) => {
+        if (myName)
+            this.myName = myName;
+        let socket = await new WebSocket(url);
+        socket.onmessage = this.handleMessage;
+        socket.onopen = () => {
+            console.log("WebSocket connection opened");
+            socket.send(
+                JSON.stringify({
+                    type: "start",
+                    data: myName,
+                })
+            );
+        };
+        socket.onclose = () => {
+            console.log("WebSocket connection closed");
+            myPeerConnection = undefined;
+            myPeerConnectionArray = [];
+            setupSignalingSocket();
+        };
+        this.socket = socket;
+    }
+    startBroadcasting = async () => {
+        try {
+            this.localStream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: {
+                    width: 320,
+                    height: 240
+                },
+            });
+            this.socket.send(
+                JSON.stringify({
+                    type: "role",
+                    data: "broadcast",
+                })
+            );
+        } catch (e) {
+            console.log(e);
+            alert('Unable to get access to your webcam and microphone.');
+        }
+    }
+    startReadingBroadcast = async () => {
+        try {
+            this.socket.send(
+                JSON.stringify({
+                    type: "role",
+                    data: "audience",
+                })
+            );
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    newPeerConnectionInstance = (target, addLocalStream = true) => {
+        const peerConnection = new RTCPeerConnection(this.myPeerConnectionConfig);
+
+        peerConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                this.socket.send(
+                    JSON.stringify({
+                        type: "new-ice-candidate",
+                        candidate: event.candidate,
+                        target,
+                    })
+                );
+            }
+        };
+
+        peerConnection.onnegotiationneeded = async () => {
+            try {
+                await peerConnection.setLocalDescription(
+                    await peerConnection.createOffer()
+                );
+                this.socket.send(
+                    JSON.stringify({
+                        type: "video-offer",
+                        sdp: peerConnection.localDescription,
+                        target,
+                        name: this.myUsername,
+                    })
+                );
+                console.log('onnegotiationneeded done');
+            } catch (e) {
+                console.log(e);
+                alert('onnegotiationneeded failed:', e);
+            }
+        };
+
+        peerConnection.ontrack = (event) => {
+            console.log("onTrack");
+            this.remoteStreamCallback(event.streams[0]);
+            this.socket.send(
+                JSON.stringify({
+                    type: "stream",
+                    data: "true",
+                })
+            );
+        };
+
+        if (addLocalStream && this.localStream)
+            this.localStream.getTracks().forEach((track) => {
+                peerConnection.addTrack(track, this.localStream);
+            });
+
+        return peerConnection;
+    };
+    createOrGetPeerConnection = (audienceName) => {
+        if (this.myPeerConnectionArray[audienceName]) return this.myPeerConnectionArray[audienceName];
+
+        this.myPeerConnectionArray[audienceName] = this.newPeerConnectionInstance(audienceName, false);
+
+        return this.myPeerConnectionArray[audienceName];
+    }
+    connectToAudience = (audienceName) => {
+        console.log('connecting to', audienceName);
+        if (!this.localStream) return;
+        if (this.myPeerConnectionArray[audienceName]) return;
+
+        this.myPeerConnectionArray[audienceName] = this.newPeerConnectionInstance(audienceName);
+    };
+    start = () => {
+        if (this.role === 'broadcast') {
+            return this.startBroadcasting();
+        }
+
+        return this.startReadingBroadcast();
+    }
+
+    constructor(role, localStreamChangeCallback, remoteStreamCallback) {
+        this.role = role;
+        this.localStreamChangeCallback = localStreamChangeCallback;
+        this.remoteStreamCallback = remoteStreamCallback;
+    }
+}
---
+++ backend/public/statics/files/frontend/js/ui.js
@@ -0,0 +1,140 @@
+const CAMERA_ON = "images/cam-on.png";
+const CAMERA_OFF = "images/cam-off.png";
+const MIC_ON = "images/mic-on.png";
+const MIC_OFF = "images/mic-off.png";
+// const SPARK_LOGO = "images/spark-logo.png";
+const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+let graph;
+let sparkRTC;
+let myName;
+let myRole;
+let shareScreenStream;
+
+function makeId(length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length));
+    }
+    return result;
+}
+
+
+function arrangeVideoContainers() {
+    const videoContainers = document.getElementById('screen')
+        .getElementsByClassName('video-container');
+    const videoCount = videoContainers.length;
+    const flexGap = 1;
+    let flexRatio = 100 / Math.ceil(Math.sqrt(videoCount));
+    let flex = "0 0 calc(" + flexRatio + "% - " + flexGap + "px)";
+
+    Array.from(videoContainers).forEach(div => {
+            div.style.setProperty('flex', flex);
+        }
+    )
+}
+
+
+function onCameraButtonClick() {
+    const img = document.getElementById("camera");
+    if (img.dataset.status === 'on') {
+        sparkRTC.disableVideo();
+        img.dataset.status = 'off';
+        img.src = CAMERA_OFF;
+    } else {
+        sparkRTC.disableVideo(true);
+        img.dataset.status = 'on';
+        img.src = CAMERA_ON;
+    }
+}
+
+
+function onMicButtonClick() {
+    const img = document.getElementById("mic");
+    if (img.dataset.status === 'on') {
+        sparkRTC.disableAudio();
+        img.dataset.status = 'off';
+        img.src = MIC_OFF;
+    } else {
+        sparkRTC.disableAudio(true);
+        img.dataset.status = 'on';
+        img.src = MIC_ON;
+    }
+}
+
+async function onShareScreen() {
+    if (!shareScreenStream) {
+        shareScreenStream = await sparkRTC.startShareScreen();
+        document.getElementById('localScreen').srcObject = shareScreenStream;
+    } else {
+        shareScreenStream.getTracks().forEach(track => track.stop());
+        shareScreenStream = null;
+        document.getElementById('localScreen').srcObject = null;
+    }
+}
+
+function setMyName() {
+    try {
+        myName = localStorage.getItem('logjam_myName');
+        document.getElementById('inputName').value = myName;
+    } catch (e) {
+        console.log(e);
+    }
+    if (myName === '' || !myName) {
+        myName = makeId(20);
+        try {
+            localStorage.setItem('logjam_myName', myName);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+function handleClick() {
+    let newName = document.getElementById("inputName").value;
+    if (newName) {
+        myName = newName;
+        localStorage.setItem('logjam_myName', myName);
+        console.log('myName=', myName);
+    }
+    document.getElementById("page").style.visibility = "visible";
+    document.getElementById("getName").style.display = "none";
+    document.getElementById("myName").innerText = newName;
+
+    return false;
+}
+
+function handleResize() {
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout(function () {
+        graph.draw(graph.treeData);
+    }, 250);
+
+}
+
+function getMyRole() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('role') === 'broadcast' ? "broadcast" : "audience";
+}
+
+function setupSignalingSocket() {
+    return sparkRTC.setupSignalingSocket(getWsUrl(), myName);
+}
+
+function start(){
+    return sparkRTC.start();
+}
+
+function onLoad() {
+    myRole = getMyRole();
+    console.log('myRole=', myRole);
+    sparkRTC = createSparkRTC();
+
+    setMyName();
+    graph = new Graph();
+    window.onresize = handleResize;
+    graph.draw(DATA);
+
+    arrangeVideoContainers();
+}
