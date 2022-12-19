09d57603f291762f6ddada44936d6541217709d7
Sal Rahman <salehen.rahman@gmail.com>
2022-12-19 08:44:48 -0800

    Laying things out

--- backend/public/statics/files/frontend/js/ui.js
+++ backend/public/statics/files/frontend/js/ui.js
@@ -14,6 +14,7 @@ let myRole;
 let shareScreenStream;
 let roomName;
 
+
 function makeId(length) {
     let result = '';
     for (let i = 0; i < length; i++) {
@@ -23,7 +24,59 @@ function makeId(length) {
 }
 
 
-let currentBackground 
+function mapping(positionsAndSizes, participants) {
+    // We have an ordered list of positions and sizes
+    //
+    // And, we have an ordered list of participants.
+    //
+    // In the extreme case, the count of pos and sizes are smaller than the
+    // count of participants. This is what we will code to.
+    //
+    // Now, not only that, but we also have a series of DOM elements. These DOM
+    // elements are not ordered according to the list of participants, and will
+    // thus need to be mapped.
+    //
+    // So the trick would be to grab the list of DOM elements, and then generate
+    // a map
+
+    // First generate a map of DOM elements and map them to participants list
+
+    const videoContainers = document.getElementById('screen')
+                .getElementsByClassName('video-container');
+
+    const videosMap = new Map();
+
+    for (const div of Array.from(videoContainers)) {
+        const id = div.querySelector('video').id.replace('localVideo-', '').replace('remoteVideo-', '');
+
+        videosMap.set(id === 'localVideo' ? div.querySelector('video').srcObject.id : id, div);
+    }
+
+    console.log(videosMap);
+
+    for (const [i, participantId] of [...participants].entries()) {
+        const div = videosMap.get(participantId) || getVideoElement('localVideo').parentElement;
+
+        div.style.setProperty('position', 'absolute');
+
+        const posSz= positionsAndSizes[i]
+        if (!!posSz) {
+            const { pos, size } = posSz;
+            console.log('Have', i, participantId)
+            div.style.setProperty('width', `${size[0]}px`)
+            div.style.setProperty('height', `${size[1]}px`)
+            div.style.setProperty('top', `${pos[1]}px`)
+            div.style.setProperty('left', `${pos[0]}px`)
+        } else {
+            console.log('Don\'t have', i, participantId)
+            div.style.setProperty('width', '200px');
+            div.style.setProperty('height', '200px');
+            div.style.setProperty('top', '0');
+            div.style.setProperty('left', '0');q
+        }
+    }
+}
+
 
 function arrangeVideoContainers() {
 
@@ -50,6 +103,50 @@ function arrangeVideoContainers() {
                 }
             }
             break;
+        case 'blue': {
+            const positionsAndSizes = [
+                { pos: [300, 10], size: [ 800, 500 ] },
+                { pos: [10, 510], size: [ 120, 120 ] },
+                { pos: [480, 310], size: [ 120, 120 ] },
+            ];
+
+            mapping(positionsAndSizes, videoLayout.meta.participants);
+
+            page.style.backgroundColor = 'blue';
+        }; break;
+        case 'red': {
+            const positionsAndSizes = [
+                { pos: [10, 10], size: [ 800, 500 ] },
+                { pos: [10, 510], size: [ 120, 120 ] },
+                { pos: [480, 510], size: [ 120, 120 ] },
+            ];
+
+            mapping(positionsAndSizes, videoLayout.meta.participants);
+
+            page.style.backgroundColor = 'red';
+        }; break;
+        case 'yellow': {
+            const positionsAndSizes = [
+                { pos: [300, 300], size: [ 480, 300 ] },
+                { pos: [10, 610], size: [ 120, 120 ] },
+                { pos: [480, 610], size: [ 120, 120 ] },
+            ];
+
+            mapping(positionsAndSizes, videoLayout.meta.participants);
+
+            page.style.backgroundColor = '#aaaa00';
+        }; break;
+        case 'purple': {
+            const positionsAndSizes = [
+                { pos: [10, 10], size: [ 480, 300 ] },
+                { pos: [10, 310], size: [ 120, 120 ] },
+                { pos: [480, 310], size: [ 120, 120 ] },
+            ];
+
+            mapping(positionsAndSizes, videoLayout.meta.participants);
+
+            page.style.backgroundColor = '#9900ff';
+        }; break;
         case 'control-freak': {
             console.log(videoLayout.meta);
             console.log('Arranging video here');
@@ -79,7 +176,7 @@ function arrangeVideoContainers() {
             }
         }; break;
         case 'tiled':
-        default:{
+        default: {
                 console.log('Arranging video here');
                 const videoContainers = document.getElementById('screen')
                     .getElementsByClassName('video-container');
@@ -233,13 +330,18 @@ let currentLayoutIndex = 0;
 const possibleLayouts = [
     { type: 'tiled', meta: null },
     { type: 'silly-frame', meta: { adminLocation: [171, 200], adminSize: [400, 400] } },
-    { type: 'control-freak', meta: { participants: [] } }
+    { type: 'control-freak', meta: { participants: [] } },
+    { type: 'blue', meta: { participants: [] } },
+    { type: 'red', meta: { participants: [] } },
+    { type: 'yellow', meta: { participants: [] } },
+    { type: 'purple', meta: { participants: [] } }
 ];
 
 
 function currentLayoutJSON() {
     return JSON.stringify(currentLayout())
 }
+
 
 function currentLayout() {
     return possibleLayouts[currentLayoutIndex]
@@ -272,12 +374,29 @@ function handleControlFreak(videoLayout) {
 }
 
 
+function handleParticipants(videoLayout) {
+    videoLayout.meta.participants = 
+        [
+            getVideoElement('localVideo').srcObject.id, 
+            ...Object.entries(sparkRTC.userStreamData).map(([ key ]) => key)
+        ];
+}
+
+
 setTimeout(() => {
     if (getMyRole() === 'broadcast') {
         const videoLayout = currentLayout();
-        if (videoLayout.type === 'control-freak') {
-            alert(JSON.stringify(sparkRTC.userStreamData))
-            handleControlFreak(videoLayout);
+
+        switch (videoLayout.type) {
+            case 'blue':
+            case 'red':
+            case 'yellow':
+            case 'purple': {
+                handleParticipants(videoLayout);
+            }; break; 
+            case 'control-freak': {
+                handleControlFreak(videoLayout);
+            }; break;
         }
 
         sparkRTC.socket.send(JSON.stringify({
@@ -318,9 +437,16 @@ document.addEventListener('keydown', (event) => {
                 (currentLayoutIndex + 1) % possibleLayouts.length;
     
             const videoLayout = currentLayout();
-            if (videoLayout.type === 'control-freak') {
-                alert(JSON.stringify(sparkRTC.userStreamData))
-                handleControlFreak(videoLayout);
+            switch (videoLayout.type) {
+                case 'blue':
+                case 'red':
+                case 'yellow':
+                case 'purple': {
+                    handleParticipants(videoLayout);
+                }; break; 
+                case 'control-freak': {
+                    handleControlFreak(videoLayout);
+                }; break;
             }
     
             sparkRTC.socket.send(JSON.stringify({
@@ -352,6 +478,36 @@ document.addEventListener('keydown', (event) => {
             data: JSON.stringify({
                 ...getMeta(),
                 backgroundLayout: currentBackgroundLayout(),
+            })
+        }))
+    }
+});
+
+window.addEventListener('message', (event) => {
+    const parsed = JSON.parse(event.data);
+
+    if (parsed.type === 'set_layout') {
+        console.log('Parsed layout', parsed);
+        currentLayoutIndex = possibleLayouts.findIndex((l) => l.type === parsed.data);
+
+        const videoLayout = currentLayout();
+        switch (videoLayout.type) {
+            case 'blue':
+            case 'red':
+            case 'yellow':
+            case 'purple': {
+                handleParticipants(videoLayout);
+            }; break; 
+            case 'control-freak': {
+                handleControlFreak(videoLayout);
+            }; break;
+        }
+
+        sparkRTC.socket.send(JSON.stringify({
+            type: 'metadata-set',
+            data: JSON.stringify({
+                ...getMeta(),
+                videoLayout: JSON.stringify(videoLayout)
             })
         }))
     }
