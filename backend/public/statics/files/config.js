36ceca0824c5e19b8045904bf58ae778232d1c0b
benyamin <43001019+benyamin218118@users.noreply.github.com>
2023-01-31 10:04:05 +0330

    replace stun/turn servers with some tmp ones for test

--- backend/public/statics/files/config.js
+++ backend/public/statics/files/config.js
@@ -1,5 +1,15 @@
 const iceServers = [
     {
+        url:"stun:65.21.156.193:3478",
+        username:"testyu",
+        credential:"testyu123"
+    },
+    {
+        url:"turn:65.21.156.193:3478",
+        username:"testyu",
+        credential:"testyu123"
+    }
+    /*{
         url: 'stun:stun.l.google.com:19302'
     },
     {
@@ -36,7 +46,7 @@ const iceServers = [
         url: "turns:turn2.turn.group.video:443",
         username: "turnuser",
         credential: "XzfVP8cpNEy17hws",
-    },
+    },*/
     // {
     //     urls: ["turn:95.215.59.228:3478"],
     //     username: "mehrdad",
