ff6671d8654e28615b246c1d5fa01e9fe1ca34cb
Mehrdad Mirsamie <m.mirsamie@gmail.com>
2023-02-02 07:57:45 +0330

    fix turn servers

--- backend/public/statics/files/config.js
+++ backend/public/statics/files/config.js
@@ -1,50 +1,50 @@
 const iceServers = [
-    // {
-    //     url: 'stun:stun.l.google.com:19302'
-    // },
-    // {
-    //     url: 'stun:stun1.l.google.com:19302'
-    // },
-    // {
-
-    //     url: 'stun:stun2.l.google.com:19302'
-    // },
-    // {
-
-    //     url: 'stun:stun3.l.google.com:19302'
-    // },
-    // {
-
-    //     url: 'stun:stun4.l.google.com:19302'
-    // },
-    // {
-    //     url: "turn:turn1.turn.group.video:3478",
-    //     username: "turnuser",
-    //     credential: "dJ4kP05PHcKN8Ubu",
-    // },
-    // {
-    //     url: "turn:turn2.turn.group.video:3478",
-    //     username: "turnuser",
-    //     credential: "XzfVP8cpNEy17hws",
-    // },
-    // {
-    //     url: "turns:turn1.turn.group.video:443",
-    //     username: "turnuser",
-    //     credential: "dJ4kP05PHcKN8Ubu",
-    // },
-    // {
-    //     url: "turns:turn2.turn.group.video:443",
-    //     username: "turnuser",
-    //     credential: "XzfVP8cpNEy17hws",
-    // },
     {
-        url: "stun:65.21.156.193:3478",
-        username: "testyu",
-        credential: "testyu123",
+        url: 'stun:stun.l.google.com:19302'
     },
     {
-        url: "turn:65.21.156.193:3478",
-        username: "testyu",
-        credential: "testyu123",
+        url: 'stun:stun1.l.google.com:19302'
     },
+    {
+
+        url: 'stun:stun2.l.google.com:19302'
+    },
+    {
+
+        url: 'stun:stun3.l.google.com:19302'
+    },
+    {
+
+        url: 'stun:stun4.l.google.com:19302'
+    },
+    {
+        url: "turn:turn1.turn.group.video:3478",
+        username: "turnuser",
+        credential: "dJ4kP05PHcKN8Ubu",
+    },
+    {
+        url: "turn:turn2.turn.group.video:3478",
+        username: "turnuser",
+        credential: "XzfVP8cpNEy17hws",
+    },
+    {
+        url: "turns:turn1.turn.group.video:443",
+        username: "turnuser",
+        credential: "dJ4kP05PHcKN8Ubu",
+    },
+    {
+        url: "turns:turn2.turn.group.video:443",
+        username: "turnuser",
+        credential: "XzfVP8cpNEy17hws",
+    },
+    // {
+    //     url: "stun:65.21.156.193:3478",
+    //     username: "testyu",
+    //     credential: "testyu123",
+    // },
+    // {
+    //     url: "turn:65.21.156.193:3478",
+    //     username: "testyu",
+    //     credential: "testyu123",
+    // },
 ];
