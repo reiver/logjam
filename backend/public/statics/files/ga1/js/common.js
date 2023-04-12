3a59d803e456822b8cf5be822fa08b2ef55e1549
benyamin <43001019+benyamin218118@users.noreply.github.com>
2023-04-12 16:10:40 +0330

    [fix] raise hand reconnection when parent is not broadcaster

--- backend/public/statics/files/ga1/js/common.js
+++ backend/public/statics/files/ga1/js/common.js
@@ -136,7 +136,11 @@ function createSparkRTC() {
             remoteStreamCallback: (stream) => {
                 const tagId = 'remoteVideo-' + stream.id;
                 if (document.getElementById(tagId)) return;
-                const video = createVideoElement(tagId);
+                let shouldMute=false;
+                if (stream.id === sparkRTC?.localStream?.id) {
+                    shouldMute = true;
+                }
+                const video = createVideoElement(tagId,shouldMute);
                 video.srcObject = stream;
                 video.play();
 
@@ -196,6 +200,26 @@ function createSparkRTC() {
                 console.log('startProcedure');
                 sparkRTC.stopSignaling();
                 clearScreen();
+                let idList=[];
+                for (const id in sparkRTC.myPeerConnectionArray) {
+                    const peerConn = sparkRTC.myPeerConnectionArray[id];
+                    await peerConn.close();
+                    idList.push(id)
+                }
+                idList.forEach((id)=>delete sparkRTC.myPeerConnectionArray[id])
+                sparkRTC.remoteStreams = [];
+                sparkRTC.localStream?.getTracks()?.forEach(track => track.stop());
+                sparkRTC.localStream = null;
+                if(sparkRTC.startedRaiseHand){
+                    img.dataset.status = 'off';
+                    img.src = RAISE_HAND_OFF;
+                    document.getElementById('mic').style.display = 'none';
+                    document.getElementById('mic').src = MIC_ON;
+                    document.getElementById('mic').dataset.status = 'on';
+                    document.getElementById('camera').style.display = 'none';
+                    document.getElementById('camera').src = CAMERA_ON;
+                    document.getElementById('camera').dataset.status = 'on';
+                }
                 await handleClick();
             },
             log: (log) => {
