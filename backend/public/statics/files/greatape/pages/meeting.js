6b0028b2a87f251cf59104a11e4d9377598ac57b
Narixius <nariman.movaffaghi@gmail.com>
2023-07-21 18:44:47 +0330

    fix: debug params value should be true only

--- backend/public/statics/files/greatape/pages/meeting.js
+++ backend/public/statics/files/greatape/pages/meeting.js
@@ -17,7 +17,9 @@ import { Roles, createSparkRTC, getWsUrl } from '../lib/common.js';
 import { GraphGenerator } from '../stats/statsGraph.js';
 
 export const isDebugMode = signal(
-    new URLSearchParams(window.location.search).get('debug')
+    (
+        new URLSearchParams(window.location.search).get('debug') || ''
+    ).toLowerCase() === 'true'
 );
 export const statsDataOpen = signal(false);
 export const statsData = signal('');
