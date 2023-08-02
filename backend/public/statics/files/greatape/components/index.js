50ea7f14ff092b71afe1e487176fb205e664f058
Narixius <nariman.movaffaghi@gmail.com>
2023-08-02 12:23:48 +0330

    fix: iPhone importmap bypass [#341]

--- backend/public/statics/files/greatape/components/index.js
+++ backend/public/statics/files/greatape/components/index.js
@@ -1,13 +1,34 @@
-export * from './Attendees/index.js';
-export * from './BottomBar/index.js';
-export * from './BottomSheet/index.js';
-export * from './Controllers/index.js';
-export * from './Dialog/index.js';
-export * from './MeetingBody/index.js';
-export * from './TopBar/index.js';
-export * from './common/Button/index.js';
-export * from './common/Container/index.js';
-export * from './common/Icon/index.js';
-export * from './common/IconButton/index.js';
-export * from './common/Logo/index.js';
-export * from './common/Tooltip/index.js';
+export {
+    Attendees,
+    AttendeesBottomSheet,
+    Participant,
+    attendees,
+    attendeesBadge,
+    attendeesCount,
+    attendeesWidth,
+    isAttendeesOpen,
+    toggleAttendees,
+} from './Attendees/index.js';
+export { BottomBar, BottomBarBottomSheet } from './BottomBar/index.js';
+export { BottomSheet } from './BottomSheet/index.js';
+export {
+    Controllers,
+    MoreControllers,
+    isMoreOptionsOpen,
+    toggleMoreOptions,
+} from './Controllers/index.js';
+export {
+    ConfirmDialog,
+    DialogPool,
+    InfoDialog,
+    ToastProvider,
+    makeDialog,
+} from './Dialog/index.js';
+export { MeetingBody, streamers } from './MeetingBody/index.js';
+export { TopBar } from './TopBar/index.js';
+export { Button } from './common/Button/index.js';
+export { Container } from './common/Container/index.js';
+export { Icon } from './common/Icon/index.js';
+export { IconButton } from './common/IconButton/index.js';
+export { Logo } from './common/Logo/index.js';
+export { Tooltip } from './common/Tooltip/index.js';
