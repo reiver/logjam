821024efaaca942c09cf23f25430b898c0e4d24b
Mian-Zaid <zaid7296@gmail.com>
2023-10-10 15:26:07 +0500

    feat: :sparkles: added sample 404 page

--- web-app/src/pages/_404.tsx
+++ web-app/src/pages/_404.tsx
@@ -1,9 +1,13 @@
+import Logo from 'assets/images/Logo + Type.svg?react'
+
 export function NotFound() {
   return (
-    <section>
-      <h1>404: Not Found</h1>
-      <p>It's gone :(</p>
-    </section>
+    <div class="w-full h-screen bg-primary flex items-center justify-center">
+      <div class="flex flex-col items-center justify-center gap-10">
+        <Logo />
+        <span class="text-medium-12">Page Not Found!</span>
+      </div>
+    </div>
   )
 }
 export default NotFound
