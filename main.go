658b72fdfacf9e5c27d33ba9524fd1dc637d9e5a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-05 22:10:56 -0800

    added a log in main()

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/reiver/logjam/cfg"
+	"github.com/reiver/logjam/srv/log"
 )
 
 func main() {
+	logsrv.Info("main", "GreatApe LogJam ⚡")
+
 	app := App{}
 
 	app.Init(cfg.Config)
