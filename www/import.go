34329cc8eaf5ec9e0d5171f780ee798f01020ad3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-12 04:38:39 -0800

    removed: www/FALLBACK www/assets www/src ; as part of removing all front-end code from logjam

--- www/import.go
+++ www/import.go
@@ -1,11 +1,8 @@
 package verboten
 
 import (
-	_ "github.com/reiver/logjam/www/assets"
 	_ "github.com/reiver/logjam/www/favicon"
 	_ "github.com/reiver/logjam/www/goldgorilla"
 	_ "github.com/reiver/logjam/www/hapi"
 	_ "github.com/reiver/logjam/www/ogimage"
-	_ "github.com/reiver/logjam/www/src"
-	_ "github.com/reiver/logjam/www/FALLBACK"
 )
