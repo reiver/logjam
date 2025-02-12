f4c2c40486a413c73970021d25a0c4be7cc27949
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-02-12 04:31:54 -0800

    removed: www/FALLBACK www/assets www/src ; as part of removing all front-end code from logjam

--- www/assets/log.go
+++
@@ -1,9 +0,0 @@
-package verboten
-
-import (
-	"github.com/reiver/logjam/srv/log"
-)
-
-const logtag string = "www/assets/server-http"
-
-var log = logsrv.Tag(logtag)
--- www/src/log.go
+++
@@ -1,9 +0,0 @@
-package verboten
-
-import (
-	"github.com/reiver/logjam/srv/log"
-)
-
-const logtag string = "www/src/server-http"
-
-var log = logsrv.Tag(logtag)
