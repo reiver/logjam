b65b11ae5636ccda385ec354fe792bca0082ca44
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-24 20:30:20 -0700

    msgs.TypeRole

--- lib/rooms/controller.go
+++ lib/rooms/controller.go
@@ -178,7 +178,7 @@ func (c *RoomWSController) Role(ctx *WSContext) {
 			}
 			if currentUser.Name != br.Name {
 				_ = c.socketSVC.Send(msgs.MessageContract{
-					Type: "role",
+					Type: msgs.TypeRole,
 					Data: "no:broadcast",
 				}, ctx.SocketID)
 				return
