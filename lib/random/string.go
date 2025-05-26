847bbf1d9793e1ef03380cf64abfdc74521e3ffd
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-25 17:17:34 -0700

    docs

--- lib/random/string.go
+++ lib/random/string.go
@@ -4,6 +4,7 @@ import "math/rand"
 
 const letterBytes = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+// String returns a string of length `size` random alpha-numberic characters.
 func String(size int) string {
 	b := make([]byte, size)
 	for i := range b {
