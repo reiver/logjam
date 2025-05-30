9a5ad42379ad93e8c0b7e768bcd32924c8c2325b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-30 16:28:49 -0700

    nodeinfo 2.0

---
+++ www/well-known/nodeinfo/2.0/servehttp.go
@@ -0,0 +1,31 @@
+package verboten
+
+import (
+	"net/http"
+
+	"github.com/reiver/go-nodeinfo"
+	nodeinfo2 "github.com/reiver/go-nodeinfo/2.0"
+	"github.com/reiver/go-path"
+
+	"github.com/reiver/logjam/cfg"
+	"github.com/reiver/logjam/srv/http"
+)
+
+func Path() string {
+	return path.Join(nodeinfo.DefaultPath, "2.0")
+}
+
+func init() {
+	var httphandler http.Handler = nodeinfo2.NodeInfo{
+		MetaData: map[string]any{
+			"nodeName":        cfg.InstanceName(),
+			"nodeDescription": cfg.InstanceDescription(),
+		},
+		Protocols: cfg.ProtocolsText(),
+		Software: nodeinfo2.Software{
+			Name: cfg.SoftwareNameText(),
+		},
+	}
+
+	httpsrv.Router.Handle(Path(), httphandler)
+}
