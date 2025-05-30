b6bff9c51b0b7b6ec22102d20379235bac144274
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-30 16:42:02 -0700

    nodeinfo

--- www/well-known/nodeinfo/servehttp.go
+++ www/well-known/nodeinfo/servehttp.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/reiver/go-nodeinfo"
-	libpath "github.com/reiver/go-path"
 	"github.com/reiver/go-opt"
 
 	"github.com/reiver/logjam/srv/http"
@@ -17,8 +16,6 @@ const path string = nodeinfo.DefaultPath
 
 func init() {
 	var httphandler http.Handler = nodeinfo.WellKnown{
-		NodeInfo1:     opt.Something(libpath.Join(nodeinfo.DefaultPath, "1.0")),
-		NodeInfo1Dot1: opt.Something(libpath.Join(nodeinfo.DefaultPath, "1.1")),
 		NodeInfo2:     opt.Something(nodeinfo2.Path()),
 		NodeInfo2Dot1: opt.Something(nodeinfo2dot1.Path()),
 		NodeInfo2Dot2: opt.Something(nodeinfo2dot2.Path()),
