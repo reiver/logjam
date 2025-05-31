0b9d147320ba43922c22ed8d3fb00968b76d7d1e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-31 07:31:58 -0700

    made it so paths in nodeinfo resolve with the host in the HTTP request

--- www/well-known/nodeinfo/servehttp.go
+++ www/well-known/nodeinfo/servehttp.go
@@ -15,13 +15,18 @@ import (
 
 const path string = nodeinfo.DefaultPath
 
+const scheme string = "https"
+
 func init() {
 	nodeinfo.SetServerText(cfg.HTTPServerText())
 
-	var httphandler http.Handler = nodeinfo.WellKnown{
-		NodeInfo2:     opt.Something(nodeinfo2.Path()),
-		NodeInfo2Dot1: opt.Something(nodeinfo2dot1.Path()),
-		NodeInfo2Dot2: opt.Something(nodeinfo2dot2.Path()),
+	var httphandler http.Handler = nodeinfo.ResolvingWellKnown{
+		WellKnown: nodeinfo.WellKnown{
+			NodeInfo2:     opt.Something(nodeinfo2.Path()),
+			NodeInfo2Dot1: opt.Something(nodeinfo2dot1.Path()),
+			NodeInfo2Dot2: opt.Something(nodeinfo2dot2.Path()),
+		},
+		Scheme: scheme,
 	}
 
 	httpsrv.Router.Handle(path, httphandler)
