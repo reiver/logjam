658357af0114994d14b8dbcd669f2a5f2ae3e32c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-06-03 04:44:10 -0700

    metadata.MetaData

---
+++ lib/metadata/metadata.go
@@ -0,0 +1,80 @@
+package metadata
+
+import (
+	"github.com/reiver/go-erorr"
+	"github.com/reiver/go-json"
+	"github.com/reiver/go-opt"
+)
+
+type MetaData struct {
+	Muted         map[string]bool
+	RecordersList []string
+	Styles        opt.Optional[string]
+}
+
+func (receiver MetaData) MarshalJSON() ([]byte, error) {
+	var buffer [512]byte
+	var p []byte = buffer[0:0]
+
+	p = append(p, '{')
+
+	{
+		const prefix string = `"muted":`
+
+		p = append(p, prefix...)
+
+		marshaled, err := json.Marshal(receiver.Muted)
+		if nil != err {
+			return nil, erorr.Errorf("problem json-marshaling MetaData.Muted: %w", err)
+		}
+		p = append(p, marshaled...)
+	}
+
+	if 0 < len(receiver.RecordersList) {
+		p = append(p, ',')
+
+		{
+			const prefix string = `"recordersList":[`
+
+			p = append(p, prefix...)
+			for index, recorder := range receiver.RecordersList {
+				if 0 < index {
+					p = append(p, ',')
+				}
+				p = append(p, json.MarshalString(recorder)...)
+			}
+			p = append(p, ']')
+		}
+	}
+
+	p = append(p, ',')
+
+	{
+		const prefix string = `"recordingStarted":`
+
+		p = append(p, prefix...)
+
+		var recordingStarted bool = (0 < len(receiver.RecordersList))
+		p = append(p, json.MarshalBool(recordingStarted)...)
+	}
+
+	p = append(p, ',')
+
+	{
+		const prefix string = `"styles":`
+
+		p = append(p, prefix...)
+
+		value, found := receiver.Styles.Get()
+		switch {
+		case found:
+			p = append(p, json.MarshalString(value)...)
+		default:
+			p = append(p, "null"...)
+		}
+	}
+
+	p = append(p, '}')
+
+	return p, nil
+}
---
+++ lib/metadata/metadata_marshaljson_test.go
@@ -0,0 +1,323 @@
+package metadata_test
+
+import (
+	"testing"
+
+	"bytes"
+
+	"github.com/reiver/go-opt"
+
+	"github.com/reiver/logjam/lib/metadata"
+)
+
+func TestMetaData_MarshalJSON(t *testing.T) {
+
+	tests := []struct{
+		MetaData metadata.MetaData
+		Expected []byte
+	}{
+		{
+			Expected: []byte(`{"muted":{},"recordingStarted":false,"styles":null}`),
+		},
+
+
+
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{
+					"30511117-4063-4b2f-8e30-99400eb5f719":false,
+				},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{`+
+						`"30511117-4063-4b2f-8e30-99400eb5f719":false`+
+					`}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{
+					"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true,
+					"30511117-4063-4b2f-8e30-99400eb5f719":false,
+				},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{`+
+						`"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true`+
+						`,`+
+						`"30511117-4063-4b2f-8e30-99400eb5f719":false`+
+					`}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{
+					"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true,
+					"30511117-4063-4b2f-8e30-99400eb5f719":false,
+					"595661ff-862b-42e6-8989-9e48158e6cf5":false,
+				},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{`+
+						`"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true`+
+						`,`+
+						`"30511117-4063-4b2f-8e30-99400eb5f719":false`+
+						`,`+
+						`"595661ff-862b-42e6-8989-9e48158e6cf5":false`+
+					`}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{
+					"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true,
+					"30511117-4063-4b2f-8e30-99400eb5f719":false,
+					"595661ff-862b-42e6-8989-9e48158e6cf5":false,
+					"8a94aaec-db1a-48b7-a892-baf2cd17c51b":false,
+				},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{`+
+						`"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true`+
+						`,`+
+						`"30511117-4063-4b2f-8e30-99400eb5f719":false`+
+						`,`+
+						`"595661ff-862b-42e6-8989-9e48158e6cf5":false`+
+						`,`+
+						`"8a94aaec-db1a-48b7-a892-baf2cd17c51b":false`+
+					`}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Muted: map[string]bool{
+					"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true,
+					"39fa5b20-6a19-4530-bc02-4d04ffce314c":true,
+					"30511117-4063-4b2f-8e30-99400eb5f719":false,
+					"595661ff-862b-42e6-8989-9e48158e6cf5":false,
+					"8a94aaec-db1a-48b7-a892-baf2cd17c51b":false,
+				},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{`+
+						`"0ed1d891-629d-44d8-9274-b8b9d1d4e6f3":true`+
+						`,`+
+						`"30511117-4063-4b2f-8e30-99400eb5f719":false`+
+						`,`+
+						`"39fa5b20-6a19-4530-bc02-4d04ffce314c":true`+
+						`,`+
+						`"595661ff-862b-42e6-8989-9e48158e6cf5":false`+
+						`,`+
+						`"8a94aaec-db1a-48b7-a892-baf2cd17c51b":false`+
+					`}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+
+
+
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{"1"},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordersList":["1"]`+
+					`,`+
+					`"recordingStarted":true`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{"1","2"},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordersList":["1","2"]`+
+					`,`+
+					`"recordingStarted":true`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{"1","2","3"},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordersList":["1","2","3"]`+
+					`,`+
+					`"recordingStarted":true`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{"1","2","3","4"},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordersList":["1","2","3","4"]`+
+					`,`+
+					`"recordingStarted":true`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				RecordersList: []string{"1","2","3","4","5"},
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordersList":["1","2","3","4","5"]`+
+					`,`+
+					`"recordingStarted":true`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+
+
+
+		{
+			MetaData: metadata.MetaData{
+				Styles: opt.Nothing[string](),
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":null`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Styles: opt.Something(""),
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":""`+
+				`}`,
+			),
+		},
+		{
+			MetaData: metadata.MetaData{
+				Styles: opt.Something(".something {\n\tfont-weight:bold;\n}\n"),
+			},
+			Expected: []byte(
+				`{`+
+					`"muted":{}`+
+					`,`+
+					`"recordingStarted":false`+
+					`,`+
+					`"styles":".something {\n\tfont-weight:bold;\n}\n"`+
+				`}`,
+			),
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actual, err := test.MetaData.MarshalJSON()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one.", testNumber)
+			t.Logf("ERROR: %s", err)
+			t.Logf("META-DATA: %#v", test.MetaData)
+			continue
+		}
+
+		expected := test.Expected
+
+		if !bytes.Equal(expected, actual) {
+			t.Errorf("For test #%d, the actual marshaled-json is not what was expected.", testNumber)
+			t.Logf("EXPECTED: (%d)\n%s", len(expected), expected)
+			t.Logf("ACTUAL:   (%d)\n%s", len(actual), actual)
+			t.Logf("META-DATA: %#v", test.MetaData)
+			continue
+		}
+	}
+}
